c801e4ec82421d5f686d322a3f834e633babc909
Joshua Rodriguez <joshua.rodriguez@flatstack.com>
2023-06-28 19:20:06 +0400

    added search behaviour to locations and episodes

--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -1,15 +1,20 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import ApiContext from "../context/ApiContext"
 import { ApiContextType, Episode } from "../types/ApiContextTypes";
 import CardsContainer from "../templates/CardsContainer";
 import EpisodeCard from "../organisms/EpisodeCard";
 import Pagination from "../molecules/Pagination";
+import SearchBox from "../molecules/SearchBox";
 
 const Episodes = () => {
 
   const { info, results, setUrl } = useContext(ApiContext) as ApiContextType;
 
   const [page, setPage] = useState<number>(1);
+
+  const [value, setValue] = useState<string>("");
+
+  const inputValue = useRef<HTMLInputElement>(null);
 
   const handleOnPrev = () => {
     setUrl(info?.prev);
@@ -25,10 +30,22 @@ const Episodes = () => {
     setUrl("https://rickandmortyapi.com/api/episode");
   }, [])
 
+  const handleOnSearch = () => {
+    const val = inputValue.current?.value as string;
+    setValue(val);
+  }
+
   return(
     <div className="px-[100px]">
+      <SearchBox inputValue={inputValue} handleOnSearch={handleOnSearch}/>
       <CardsContainer>
-        {results?.map(val => <EpisodeCard episode={val as Episode}/>)}
+        {results?.filter( val => {
+          const episode = val as Episode;
+          const filterValue = value.toLowerCase();
+          const episodeName = episode.name.toLowerCase();
+
+          return episodeName.includes(filterValue);
+        }).map(val => <EpisodeCard episode={val as Episode}/>)}
       </CardsContainer>
       <Pagination info={info} page={page} handleOnPrev={handleOnPrev} handleOnNext={handleOnNext} />
     </div>
--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,18 +1,22 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import ApiContext from "../context/ApiContext";
 import { ApiContextType, Location } from "../types/ApiContextTypes";
 import LocationCard from "../organisms/LocationCard";
 import CardsContainer from "../templates/CardsContainer";
 import Pagination from "../molecules/Pagination";
+import SearchBox from "../molecules/SearchBox";
 
 const Locations = () => {
   
   useEffect(()=>{
     setUrl("https://rickandmortyapi.com/api/location")
   }, [])
+
+  const inputValue = useRef<HTMLInputElement>(null)
   
   const { results, info, setUrl } = useContext(ApiContext) as ApiContextType;
   const [page, setPage] = useState<number>(1);
+  const [value, setValue] = useState<string>("")
 
   const handleOnNext = () => {
     setUrl(info?.next);
@@ -24,12 +28,24 @@ const Locations = () => {
     setPage(prev => prev - 1)
   }
 
+  const handleOnSearch = () => {
+    const val = inputValue.current?.value as string;
+    setValue(val);
+  }
+
   
   return (
     <>
       <div className="px-[60px]">
+        <SearchBox inputValue={inputValue} handleOnSearch={handleOnSearch} />
         <CardsContainer>
-          { results?.map(val => <LocationCard location={val as Location} />)}
+          { results?.filter((val)=>{
+            let loc = val as Location;
+            let nameLowed = loc.name.toLowerCase();
+            let valLowed = value.toLowerCase();
+            return nameLowed.includes(valLowed);
+
+          }).map(val => <LocationCard location={val as Location} />)}
         </CardsContainer>
         <Pagination info={info} page={page} handleOnNext={handleOnNext} handleOnPrev={handleOnPrev}/>
       </div>
